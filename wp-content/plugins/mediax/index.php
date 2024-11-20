c4898c1eba0dbda2d5f4f32cc93dbe491fecabac
SanjanaNVISION <isurus@nvision.lk>
2024-11-20 18:46:35 +0530

    WP to Git

---
+++ wp-content/plugins/mediax/index.php
@@ -0,0 +1,9 @@
+/**
+* Plugin Name: MediaX
+* Plugin URI: https://mediaxgo.com/
+* Description: Registration for Advertisment Campaigns via MediaX portal.
+* Version: 2.0
+* Author: M.G.I.Sanjana
+* Author URI: https://mediaxgo.com/
+* License: GPL2
+*/
